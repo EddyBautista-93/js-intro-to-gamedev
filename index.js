f91fdd64572a6e43d5d751bec22ffe6c0a4432b6
EddyBautista <bautista.j.eddy@gmail.com>
2021-10-24 09:36:25 -0500

    draw objects into canvas

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Testing testing 1231432");
+let canvas = document.getElementById("gameScreen");
+let ctx = canvas.getContext('2d');
+
+// clear a screen every frame
+ctx.clearRect(0, 0, 800, 600);
+
+
+// drawing out a object
+ctx.fillStyle = '#f00'
+ctx.fillRect(20, 20, 100, 100);
+
+
